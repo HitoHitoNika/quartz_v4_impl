4270c328ddeb67b61a8896338fbc340734139eff
HitoHitoNika <mbalthasar0@gmail.com>
2023-09-11 21:05:38 +0200

    Quartz sync: Sep 11, 2023, 9:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Max sein Wiki",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://hitohitonika.github.io/quartz_v4_impl/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
