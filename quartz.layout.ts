168ddb53b24f3fc57f6a8b383ae3d6bf9cf56d74
HitoHitoNika <mbalthasar0@gmail.com>
2023-09-23 03:09:32 +0200

    Explorer readdet (Ich inte?)

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,7 +21,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
